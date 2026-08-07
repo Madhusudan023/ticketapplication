0b5e56559d6e39b5fbfe403c4c0b58ad5eb1ae8e
Pranathi Jinnaram <pranathi.jinnaram103@gmail.com>
2026-08-07 12:38:44 +0530

    Update outputs and deploy workflow to complete 100% AWS Free Tier deployment setup

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,20 @@
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.main.dns_name
+output "ec2_public_ip" {
+  description = "Public IP of the Free Tier EC2 Instance"
+  value       = aws_instance.app_server.public_ip
 }
 
-output "eureka_url" {
-  description = "Eureka Dashboard URL via ALB"
-  value       = "http://${aws_lb.main.dns_name}:8761"
+output "free_tier_eureka_url" {
+  description = "Eureka Dashboard URL on Free Tier EC2"
+  value       = "http://${aws_instance.app_server.public_ip}:8761"
+}
+
+output "free_tier_api_gateway_url" {
+  description = "API Gateway URL on Free Tier EC2"
+  value       = "http://${aws_instance.app_server.public_ip}:8080"
 }
 
 output "frontend_s3_url" {
-  description = "Frontend S3 website URL"
+  description = "Frontend S3 Website URL"
   value       = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
 }
 
